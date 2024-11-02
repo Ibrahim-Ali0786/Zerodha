038267d310f72d8e4b845ec73dc05caf5f8b7c88
IbrahimAli <ibrahimtin0786@gmail.com>
2024-11-02 19:57:04 +0530

    some changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,24 +7,28 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 const Home = () => {
   const navigate = useNavigate();
-  const [cookies, removeCookie] = useCookies(['token']);
+  const [cookies, removeCookie] = useCookies([]);
   const [username, setUsername] = useState("");
+  console.log(cookies)
   useEffect(() => {
     const verifyCookie = async () => {
       if (!cookies.token) {
-      //  window.location.href="https://frontend.d1dk8zlerjmfx7.amplifyapp.com/login";
-      navigate('/')
-      console.log(cookies.token)
-      }
-      const { data } = await axios.post(
-        "https://zerodha-pq9f.onrender.com",
-        { withCredentials: true },
-      );
-      const { status, user } = data;
-      setUsername(user);
-      return status
-        ? navigate('/')
-        : navigate('/')
+        // window.location.href="https://frontend.d1dk8zlerjmfx7.amplifyapp.com/login";
+        console.log(cookies)
+        navigate('/')
+       }
+       const { data } = await axios.post(
+         "https://zerodha-pq9f.onrender.com",
+         {},
+         { withCredentials: true }
+       );
+       console.log(data)
+       const { status, user } = data;
+       setUsername(user);
+       return status
+         ? navigate('/')
+        //  : (removeCookie("token"), window.location.href="https://frontend.d1dk8zlerjmfx7.amplifyapp.com/login");
+        :navigate('/')
     };
     verifyCookie();
   },[]);
