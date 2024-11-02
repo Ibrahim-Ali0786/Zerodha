a9254c22c539d4b98efb1244d732d11014f45952
IbrahimAli <ibrahimtin0786@gmail.com>
2024-11-02 17:23:53 +0530

    some changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,7 +12,9 @@ const Home = () => {
   useEffect(() => {
     const verifyCookie = async () => {
       if (!cookies.token) {
-       window.location.href="https://frontend.d1dk8zlerjmfx7.amplifyapp.com/login";
+      //  window.location.href="https://frontend.d1dk8zlerjmfx7.amplifyapp.com/login";
+      navigate('/');
+      console.log(cookies)
       }
       const { data } = await axios.post(
         "https://zerodha-pq9f.onrender.com",
