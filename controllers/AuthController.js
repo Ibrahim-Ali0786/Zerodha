bd92f1428fd80a903942ebfd6cfbb2eadd47fec7
IbrahimAli <ibrahimtin0786@gmail.com>
2024-11-02 16:55:07 +0530

    some changes

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -15,8 +15,7 @@ module.exports.Signup = async (req, res, next) => {
       httpOnly: true,
       secure: true,
       sameSite: "None",
-      domain: 'dashboard.d3bnl1cz0kxf11.amplifyapp.com',
-      path: '/',
+      path: '*',
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
     });
     res
@@ -46,8 +45,7 @@ module.exports.Login = async (req, res, next) => {
         httpOnly: true,
         secure: true,
         sameSite: "None",
-        domain: 'dashboard.d3bnl1cz0kxf11.amplifyapp.com',
-        path: '/',
+        path: '*',
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
       });
        res.status(201).json({ message: "User logged in successfully", success: true });
